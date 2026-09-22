1d31ef9306768f169ecd6fe0154f480c25cd6a3d
adrian <adrianjosueamayasanchez818@gmail.com>
2026-09-22 07:59:46 -0500

    entrega parcial

--- src/Ejercicio1.java
+++ src/Ejercicio1.java
@@ -7,19 +7,31 @@ public class Ejercicio1 {
 
         int consumo[] = new int[10];
         
-       
+        int consutotal = 0;
+        int mayor = 0;
         int longitud = consumo.length;
         for (int i = 0; i < longitud; i++) {
             System.out.println("ingrese el consumo del agua del sector " + (i+1));
             consumo[i] = scanner.nextInt();
-             int consutotal = 0;
+            
             if (consumo[i] >= 0 ) {
                 consutotal += consumo[i];
+                if (consumo[i] > mayor) {
+                    mayor = consumo[i];
+                }
             }else{
                 System.out.println("valor invalido");
             }
                 
-            }
         }
-    }
+        System.out.println("el mayor consumo total es de: " + mayor);
+        System.out.println("el consumo total es de: " + consutotal);
+        double promedio = consutotal/longitud;
+         System.out.println("el promedio es de: " + promedio);
 
+        scanner.close();
+    }
+         
+}
+
+ 
--- src/Ejercicio2.java
+++ src/Ejercicio2.java
@@ -1,5 +1,26 @@
+import java.util.Scanner;
+
 public class Ejercicio2 {
     public static void main(String[] args) {
-        
+       
+         Scanner scanner = new Scanner(System.in);
+    int matriz[][] =  new int[4][5];
+
+       int longitud = matriz.length;
+       for (int i = 0; i < longitud; i++) {
+           for (int j = 0; j < matriz[i].length; j++) {
+               System.out.println("ingrese la produdcion de la maquina");
+               matriz[i][j] = scanner.nextInt();
+           }
+        }
+        for (int i = 0; i < longitud; i++) {
+           for (int j = 0; j < matriz[i].length; j++) {
+               System.out.print( matriz[i][j]);
+           }
+           System.out.println();
+        }
+
+
+      scanner.close();
     }
 }

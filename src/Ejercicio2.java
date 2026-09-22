747f94a668507513e0fdc21622613dc133670d87
adrian <adrianjosueamayasanchez818@gmail.com>
2026-09-22 08:05:39 -0500

    entrega parcial 1

--- src/Ejercicio2.java
+++ src/Ejercicio2.java
@@ -6,20 +6,28 @@ public class Ejercicio2 {
          Scanner scanner = new Scanner(System.in);
     int matriz[][] =  new int[4][5];
 
+        int total = 0;
+        int mayor = 0;
        int longitud = matriz.length;
        for (int i = 0; i < longitud; i++) {
            for (int j = 0; j < matriz[i].length; j++) {
                System.out.println("ingrese la produdcion de la maquina");
                matriz[i][j] = scanner.nextInt();
+               if (matriz[i][j] > mayor) {
+                 mayor = matriz[i][j];
+               }
+               total +=  matriz[i][j];
            }
         }
         for (int i = 0; i < longitud; i++) {
            for (int j = 0; j < matriz[i].length; j++) {
-               System.out.print( matriz[i][j]);
+               System.out.print( "[" + matriz[i][j] + "] ");
            }
            System.out.println();
         }
-
+        
+        System.out.println("la mayor producido es de:" + mayor);
+        System.out.println("el total producido es de:" + total);
 
       scanner.close();
     }
